d9094a1e879ac8d36a271cb4e78a1786dd75fcbc
Hiro Nakae <hnakae@uoregon.edu>
2026-03-12 23:06:08 -0700

    vercel bug fix

--- lib/generated/zod/index.ts
+++ lib/generated/zod/index.ts
@@ -8,7 +8,7 @@ import { Prisma } from '../prisma/client';
 // JSON
 //------------------------------------------------------
 
-export type NullableJsonInput = Prisma.JsonValue | null | 'JsonNull' | 'DbNull' | Prisma.NullTypes.DbNull | Prisma.NullTypes.JsonNull;
+export type NullableJsonInput = Prisma.JsonValue | null | 'JsonNull' | 'DbNull' | typeof Prisma.NullTypes.DbNull | typeof Prisma.NullTypes.JsonNull;
 
 export const transformJsonNull = (v?: NullableJsonInput) => {
   if (!v || v === 'DbNull') return Prisma.NullTypes.DbNull;
--- prisma.config.ts
+++ prisma.config.ts
@@ -16,7 +16,6 @@ export default defineConfig({
   migrations: {
     path: "prisma/migrations",
   },
-  engine: "classic",
   datasource: {
     url: databaseUrl,
   },
